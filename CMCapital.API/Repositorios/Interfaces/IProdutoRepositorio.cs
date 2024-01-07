f69c2f58ee9a3e82803d36e5c8ead743f50d7624
Luis <luis.r.s14@hotmail.com>
2024-01-07 02:01:29 -0300

    feat: criado repositorio de produto

---
+++ CMCapital.API/Repositorios/Interfaces/IProdutoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using CMCapital.API.Entidades;
+using System.Linq.Expressions;
+
+namespace CMCapital.API.Repositorios.Interfaces;
+
+public interface IProdutoRepositorio
+{
+    Task<Produto?> ObterPorDescricao(string descricao);
+    Task<Produto?> Criar(Produto produto);
+    Task<Produto?> Atualizar(int codigo, Produto produto);
+    Task Deletar(int codigo);
+    Task<Produto?> ObterPorQuery(Expression<Func<Produto, bool>> query);
+}
