6b2571f7ec90adc8ef9bdeb85e64812ac51ce388
Luis <luis.r.s14@hotmail.com>
2024-01-07 17:06:07 -0300

    feat: adicionado validadores ao criar cliente e produto

---
+++ CMCapital.API/DTOs/Validadores/ClienteDTOValidador.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace CMCapital.API.DTOs.Validadores;
+
+public class ClienteDTOValidador : AbstractValidator<ClienteDTO>
+{
+    public ClienteDTOValidador()
+    {
+        RuleFor(x => x.Nome)
+            .NotNull().WithMessage("O nome não pode ser nulo")
+            .NotEmpty().WithMessage("O nome não pode estar vazio")
+            .MinimumLength(4).WithMessage("O nome deve ter no minimo 4 caracteres");
+
+        RuleFor(x => x.Telefone)
+            .NotNull().WithMessage("O telefone não pode ser nulo")
+            .NotEmpty().WithMessage("O telefone não pode estar vazio")
+            .MinimumLength(11).WithMessage("O nome deve ter no minimo 11 caracteres");
+
+        RuleFor(x => x.CapacidadeComprar)
+            .NotNull().WithMessage("A capacidade de compra não pode ser nula");
+    }
+}
---
+++ CMCapital.API/DTOs/Validadores/ProdutoDTOValidador.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace CMCapital.API.DTOs.Validadores;
+
+public class ProdutoDTOValidador : AbstractValidator<ProdutoDTO>
+{
+    public ProdutoDTOValidador()
+    {
+        RuleFor(x => x.Descricao)
+            .NotNull().WithMessage("A descrição não pode ser nula.")
+            .NotEmpty().WithMessage("A descrição não pode estar vazia.")
+            .MinimumLength(5).WithMessage("A descrição deve ter no minimo 5 caracteres.");
+
+        RuleFor(x => x.ValorUnitario)
+            .NotNull().WithMessage("O valor unitário não pode ser nulo.");
+    }
+}
